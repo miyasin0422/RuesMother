9ee5d83e7638630be8b449acf5afee2781fcf43c
miyasin0422 <sota150422@docomo.ne.jp>
2026-08-06 22:24:13 +0900

    シーン移動の修正

--- Assets/Scripts/Stage/SceneChange.cs
+++ Assets/Scripts/Stage/SceneChange.cs
@@ -11,6 +11,7 @@ public class SceneChange : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("aaa");
         if (other.CompareTag("Player"))
         {
             SceneMoveData.NextSpawnId = nextSpawnId;
